ac540cfe406b234b0184c7668222f38ac170bb8f
srinu1085 <84057026+srinu1085@users.noreply.github.com>
2021-12-03 14:40:40 +0530

    Create leetcode141.cpp

---
+++ leetcode141.cpp
@@ -0,0 +1,17 @@
+ if(head==NULL || head->next==NULL){
+            return false;
+        }
+        ListNode *low=head;
+        ListNode *high=head;
+        while(low->next && low->next->next){
+            low=low->next;
+            if(low==high){
+                return true;
+            }
+            low=low->next;
+            if(low==high){
+                return true;
+            }
+            high=high->next;
+        }
+        return false;
